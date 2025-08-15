c3c7e3f718a35fd346e5577950dc9424f227b56d
mpearc11 <mpearc11@jhu.edu>
2025-08-15 08:57:00 -0400

    Add files via upload

--- module4-2streamlit.py
+++ module4-2streamlit.py
@@ -5,9 +5,13 @@ from bokeh.io import show
 import streamlit as st
 from Bio.Seq import Seq
 from Bio import SeqIO
+from Bio import Align
 from io import StringIO
 
-uploaded_file = st.file_uploader("",type='fasta')
+aligner = Align.PairwiseAligner()
+aligner.mode = 'local'
+
+uploaded_file = st.file_uploader("",type='fasta',key=1)
 
 if uploaded_file is not None:
     st.success("nanopore sequence file uploaded")
@@ -16,19 +20,31 @@ else:
 
 if st.button('annotate sequence'):
     stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
-    st.write(stringio)
-    print(type(stringio))
-    #npseq = uploaded_file.read()
-    #st.write(npseq)
     record = SeqIO.read(stringio, 'fasta')
-    st.write(record)
-    npseq = Seq(str(stringio))
+    npseq = str(record.seq)
 
-    # get pandas df of annotations
-    hits = annotate(npseq, is_detailed = True, linear= True)
+# get pandas df of annotations
+hits = annotate(npseq, is_detailed = True, linear= True)
 
-# get biopython SeqRecord object
-seq_record = get_seq_record(hits, npseq)
+# get biopython SeqRecord object; this is already done earlier i think
+#seq_record = get_seq_record(hits, npseq)
 
 # show plot
 show(get_bokeh(hits, linear=True))
+
+@st.fragment()
+def alignseqs():
+    uploaded_file2 = st.file_uploader("",type='fasta',key=2)
+
+    if uploaded_file2 is not None:
+        st.success("original target sequence file uploaded")
+    else:
+        st.info("please upload your target sequence file")
+
+    if st.button('align sequences'):
+        stringio = StringIO(uploaded_file2.getvalue().decode('utf-8'))
+        record = SeqIO.read(stringio, 'fasta')
+        ogseq = str(record.seq)
+        alignment = aligner.align(npseq,ogseq)
+        st.write(alignment[0])
+alignseqs()
