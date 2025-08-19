5bcfab780e00311d336388be70495a679bd505ad
mpearc11 <mpearc11@jhu.edu>
2025-08-18 23:14:36 -0400

    Update module4-2streamlit.py

--- module4-2streamlit.py
+++ module4-2streamlit.py
@@ -5,7 +5,7 @@ import sys
 subprocess.run([f"{sys.executable}", "annotate.py"])
 
 #below is sufficient for running locally
-from plannotate.annotate import annotate
+#from plannotate.annotate import annotate
 from plannotate.bokeh_plot import get_bokeh
 from plannotate.resources import get_seq_record
 from bokeh.io import show
